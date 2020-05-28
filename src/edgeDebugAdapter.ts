51948c1609734ebc00dc24fa62a5f0b0643f7925
Courtney Owen <cowen@microsoft.com>
2020-05-28 11:58:13 -0700

    Check that user is running Windows 1903 or greater
    - Previously we just checked if the user is running Windows 10 but this doesn't tell us enough to determine if user is running Edge webview so now checking that user is running Windows 1903 or greater

--- src/edgeDebugAdapter.ts
+++ src/edgeDebugAdapter.ts
@@ -9,6 +9,7 @@ import  { getAdapterPath, getSourceMapPathOverrides, usageDataObject} from './ut
 import * as childProcess from 'child_process';
 import * as path from 'path';
 import * as fs from 'fs';
+import * as os from 'os';
 
 export class EdgeDebugAdapter extends ChromeDebugAdapter {
     private _adapterProc: childProcess.ChildProcess;
@@ -24,15 +25,22 @@ export class EdgeDebugAdapter extends ChromeDebugAdapter {
 
         // Check that debug adpater executable exists
         if (!fs.existsSync(adapterExePath)) {
-            if (utils.getPlatform() == utils.Platform.Windows) {
+            if (utils.getPlatform() == utils.Platform.Windows && os.release() >= '10.0.18362') {
                 const error: string = "No Edge Diagnostics Adapter was found. Install the Edge Diagnostics Adapter (https://github.com/OfficeDev/debug-adapter-for-office-addins) and specify a valid 'adapterExecutable' path";
                 usageDataObject.sendUsageDataException("_launchAdapter", error);
                 return utils.errP(error);
             } else {
-                const error: string = "Edge debugging is only supported on Windows 10.";
+                const error: string = "Edge debugging is only supported on Windows 10 version 1903 (build 10.0.18362) and greater.";
                 usageDataObject.sendUsageDataException("_launchAdapter", error);
                 return utils.errP(error);
             }
+        }
+
+        // Check that user is running Windows 1903 (build 10.0.18362) or greater to ensure Edge webview is being used
+        if (os.release() < '10.0.18362') {
+            const error: string = `Edge debugging is only supported on Windows 10 version 1903 (build 10.0.18362) and greater.  Currently installed version is ${os.release()}`;
+            usageDataObject.sendUsageDataException("_launchAdapter", error);
+            return utils.errP(error);
         }
 
         // Check that user is running a supported version of NodeJs (10 or higher)
